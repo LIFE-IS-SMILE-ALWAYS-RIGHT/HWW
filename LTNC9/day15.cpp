55777e9dc954c41a971223a7fbaedbe7b8eda315
LIFE-IS-SMILE-ALWAYS-RIGHT <160494904+LIFE-IS-SMILE-ALWAYS-RIGHT@users.noreply.github.com>
2024-05-07 20:36:57 +0700

    Create day15.cpp

---
+++ LTNC9/day15.cpp
@@ -0,0 +1,10 @@
+ Node* insert(Node *head,int data)
+{
+    //Complete this method
+    if(head==NULL){
+        Node* tmp = new Node(data);
+        return tmp ;
+    }
+    head->next = insert(head->next,data);
+    return head;
+}
